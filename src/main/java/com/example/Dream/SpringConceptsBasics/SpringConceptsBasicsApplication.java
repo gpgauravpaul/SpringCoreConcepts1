3e2d279b0ecc1f4535fed8217f225ead2937fbcb
Gaurav Kumar <gaurav.kumar23@globallogic.com>
2022-08-30 10:56:51 +0530

    first commit

---
+++ src/main/java/com/example/Dream/SpringConceptsBasics/SpringConceptsBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dream.SpringConceptsBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptsBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConceptsBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Dream/SpringConceptsBasics/SpringConceptsBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dream.SpringConceptsBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConceptsBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
